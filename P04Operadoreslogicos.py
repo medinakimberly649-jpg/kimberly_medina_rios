d88738ecd94274bd6029318036b9a120b3b1f5bf
medinakimberly649-jpg <medinakimberly649@gmail.com>
2026-04-19 09:20:40 -0600

    Delete P04Operadoreslogicos.py

--- P04Operadoreslogicos.py
+++
@@ -1,7 +0,0 @@
-edad = int(input("Ingresa tu edad: "))
-tiene_identificacion = input("¿Tienes identificación? (si/no): ")
-
-if edad >= 18 and tiene_identificacion == "si":
-    print("Puedes entrar")
-else:
-    print("No puedes entrar")
