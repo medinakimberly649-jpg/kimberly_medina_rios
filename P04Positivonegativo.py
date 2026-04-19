237a12c9feea4771d820b2cf0f5739fe8feefdbe
medinakimberly649-jpg <medinakimberly649@gmail.com>
2026-04-19 00:26:37 -0600

    Add files via upload

---
+++ P04Positivonegativo.py
@@ -0,0 +1,10 @@
+# Kimberly Medina Ríos
+
+numero = float(input("Ingresa un número: "))
+
+if numero > 0:
+    print("El número es positivo")
+elif numero < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
