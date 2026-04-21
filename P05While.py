81eac67c99b4b401d6f7222c620e1378eacfb2d1
medinakimberly649-jpg <medinakimberly649@gmail.com>
2026-04-21 17:36:19 -0600

    Add files via upload

---
+++ P05While.py
@@ -0,0 +1,8 @@
+# Kimberly Medina Ríos
+# # Ejemplo básico de ciclo While
+
+contador = 1
+
+while contador <= 5:   # mientras contador sea menor o igual a 5
+    print("Este es el mensaje número", contador)
+    contador += 1      # aumenta el contador en 1
