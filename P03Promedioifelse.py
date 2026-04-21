6885de607454bc9b37dbc12b8c49f8e440e24741
medinakimberly649-jpg <medinakimberly649@gmail.com>
2026-04-21 17:31:38 -0600

    Add files via upload

---
+++ P03Promedioifelse.py
@@ -0,0 +1,12 @@
+# Kimberly Medina Ríos
+
+cal1 = float(input("Calificación 1: "))
+cal2 = float(input("Calificación 2: "))
+cal3 = float(input("Calificación 3: "))
+
+promedio = (cal1 + cal2 + cal3) / 3
+
+if promedio >= 6:
+    print("Aprobado")
+else:
+    print("Reprobado")
