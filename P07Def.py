20e8dff47296000a8ccc7ea96637cf20a8dc8473
medinakimberly649-jpg <medinakimberly649@gmail.com>
2026-04-21 17:38:24 -0600

    Add files via upload

---
+++ P07Def.py
@@ -0,0 +1,8 @@
+# Kimberly Medina Ríos
+# Definimos una función que repite un mensaje
+def repetir_mensaje(mensaje, veces):
+    for i in range(veces):
+        print(mensaje)
+
+# Llamamos a la función
+repetir_mensaje("Hola, bienvenido al programa", 5)
